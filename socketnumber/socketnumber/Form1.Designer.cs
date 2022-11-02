edf33b16d85324c70b3b96c0d075d91b9af0a9cd
MatD3MI <76449947+MatD3MI@users.noreply.github.com>
2022-11-02 13:45:24 +0100

    esNumberCS

---
+++ socketnumber/socketnumber/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace socketnumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connetti = new System.Windows.Forms.Button();
+            this.textBoxReceive = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // connetti
+            // 
+            this.connetti.Location = new System.Drawing.Point(156, 69);
+            this.connetti.Name = "connetti";
+            this.connetti.Size = new System.Drawing.Size(75, 23);
+            this.connetti.TabIndex = 0;
+            this.connetti.Text = "INVIA ";
+            this.connetti.UseVisualStyleBackColor = true;
+            this.connetti.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxReceive
+            // 
+            this.textBoxReceive.Location = new System.Drawing.Point(110, 98);
+            this.textBoxReceive.Multiline = true;
+            this.textBoxReceive.Name = "textBoxReceive";
+            this.textBoxReceive.Size = new System.Drawing.Size(166, 63);
+            this.textBoxReceive.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 242);
+            this.Controls.Add(this.textBoxReceive);
+            this.Controls.Add(this.connetti);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connetti;
+        private System.Windows.Forms.TextBox textBoxReceive;
+    }
+}
+
---
+++ socketnumber/socketnumber/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace socketnumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] bytes = new byte[1024];
+
+            // Connect to a remote device.  
+            try
+            {
+                // Establish the remote endpoint for the socket.  
+                // This example uses port 11000 on the local computer.  
+                IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 5000);
+
+                // Create a TCP/IP  socket.  
+                Socket SOCKET = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.  
+                try
+                {
+                    SOCKET.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        SOCKET.RemoteEndPoint.ToString());
+
+                    // Encode the data string into a byte array.  
+                    byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>"); //messaggio che mando al server
+
+                    // Send the data through the socket.  
+                    int bytesSENT = SOCKET.Send(msg);  //effettivo invio
+
+                    // Receive the response from the remote device.  
+                    int bytesRECEIVE = SOCKET.Receive(bytes);
+                    Console.WriteLine("Echoed test = {0}",
+                      
+                    textBoxReceive.Text=Encoding.ASCII.GetString(bytes, 0, bytesRECEIVE));
+
+                    // Release the socket.  
+                    SOCKET.Shutdown(SocketShutdown.Both);
+                    SOCKET.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception a)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", a.ToString());
+                }
+
+            }
+            catch (Exception a)
+            {
+                Console.WriteLine(a.ToString());
+            }
+        }
+
+
+    }
+}
